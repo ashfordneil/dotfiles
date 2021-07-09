a3db4569c2e518e3139c123b854db155c5dbf3f4
Neil Ashford <neil.ashford@tiny.cloud>
2021-07-09 17:07:52 +1000

    Fix typo in init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local execute = vim.api.nvim_command
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com.wbthomason/packer.nvim '..install_path)
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
 end
 
 local use = require('packer').use
